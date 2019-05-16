e5032ae87f7cb2935ae182512aa2be1fb0149955
azheltishev <azheltishevs@gmail.com>
2019-05-16 17:40:40 +0300

    shift down + random spawn on free place

--- main.go
+++ main.go
@@ -21,13 +21,86 @@ func main() {
 
 	cellSize := 4
 
+	// x, y := rand.Intn(4), rand.Intn(4)
+	// val := rand.Intn(2) + 1
+
+	// field[x][y] = val
+
+	for {
+		field = randomlySpawn(field)
+		drawField(field, cellSize)
+		t.Flush()
+		e := t.PollEvent()
+		if e.Type == t.EventKey {
+
+			if e.Ch == 'q' {
+				break
+			}
+
+			switch e.Key {
+			case t.KeyArrowUp:
+
+			case t.KeyArrowDown:
+				field = shiftRowsDown(field)
+			case t.KeyArrowLeft:
+
+			case t.KeyArrowRight:
+
+			}
+		}
+	}
+
+	log.Println("good bye!")
+}
+
+func randomlySpawn(field [4][4]int) [4][4]int {
 	x, y := rand.Intn(4), rand.Intn(4)
-	val := rand.Intn(2) + 1
 
-	field[x][y] = val
+	for field[x][y] != 0 {
+		x, y = rand.Intn(4), rand.Intn(4)
+	}
 
-	drawField(field, cellSize)
-	t.Flush()
+	field[x][y] = rand.Intn(2) + 1
+	return field
+}
+
+func shiftRowsDown(field [4][4]int) [4][4]int {
+	for times := 0; times < 4; times++ {
+		for y := 3; y > 0; y-- {
+			for x := 0; x < 4; x++ {
+				if field[x][y] == 0 {
+					if field[x][y-1] != 0 {
+						if field[x][y-1] == field[x][y] {
+							field[x][y] = -(field[x][y] + 1)
+							field[x][y-1] = 0
+						} else {
+							field[x][y] = field[x][y-1]
+							field[x][y-1] = 0
+						}
+					}
+				}
+			}
+		}
+	}
+	return mod(field)
+}
+
+func mod(field [4][4]int) [4][4]int {
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			if field[i][j] < 0 {
+				field[i][j] = -field[i][j]
+			}
+		}
+	}
+	return field
+}
+
+func shiftCols(yd int, field [4][4]int) [4][4]int {
+	// for row := 0; row < 4; row++ {
+
+	// }
+	return field
 }
 
 func drawField(field [4][4]int, cellSize int) {
